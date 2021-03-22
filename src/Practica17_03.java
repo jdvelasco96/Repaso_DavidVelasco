918a99debc2c7ad87c1200eaff714c000cdb6bf2
Alexander Pichard <39698441+alexanderpichard@users.noreply.github.com>
2021-03-22 08:51:39 +0100

    este es la rama de alexander

--- src/Practica17_03.java
+++ src/Practica17_03.java
@@ -1,21 +1,20 @@
-import java.util.Scanner;
+
 
 public class Practica17_03 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("esto es una calculadora basica de Juan :D");
+		System.out.println("La suma es: " + Practica17_03.sumar(7,3));
 		
-		sumar();
 	
 
 	}
 	
 	
-
-	public static int sumar() {
+	public static int sumar(int n1, int n2) {
 		
-		return 0;
+		return n1+n2;
 	}
 
 }
